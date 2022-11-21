a96353699f8967fe95bc8288b8812cadf588ef61
zephyrLiao <1162382489@qq.com>
2022-11-21 11:25:06 +0800

    learn how to commit v1

---
+++ webbase/DubboParent/DubboProvider/src/main/java/org/lwq/ProviderStarter.java
@@ -0,0 +1,13 @@
+package org.lwq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+public class ProviderStarter {
+
+    public static void main(String[] args) {
+        ConfigurableApplicationContext context = SpringApplication.run(ProviderStarter.class, args);
+    }
+}
